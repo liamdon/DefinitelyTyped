c13083fb5929c15fcaca12f25517f93b8d82d07e
Liam Don <liamdon@gmail.com>
2020-04-26 14:31:26 -0700

    [javascript-state-machine] Update definitions to 3.0.1

--- types/javascript-state-machine/index.d.ts
+++ types/javascript-state-machine/index.d.ts
@@ -1,105 +1,85 @@
-// Type definitions for Finite State Machine 2.4
+// Type definitions for Finite State Machine 3.0
 // Project: https://github.com/jakesgordon/javascript-state-machine
 // Definitions by: Boris Yankov <https://github.com/borisyankov>,
 // 					Maarten Docter <https://github.com/mdocter>,
 // 					William Sears <https://github.com/MrBigDog2U>,
 // 					samael <https://github.com/samael65535>,
-// 					taoqf <https://github.com/taoqf>
+// 					taoqf <https://github.com/taoqf>,
+// 					Liam Don <https://github.com/liamdon>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export type StateMachineErrorCallback = (
-	eventName?: string,
-	from?: string,
-	to?: string,
-	args?: any[],
-	errorCode?: number,
-	errorMessage?: string,
-	ex?: Error
-) => void; // NB. errorCode? See: StateMachine.Error
-
-export interface StateMachineEventDef {
-	name: string;
-	from: any;    // string or string[]
-	to: string;
+export interface StateMachineTransitionsConfigItem {
+    name: string;
+    from: string | string[];
+    to: string;
 }
 
-export type StateMachineEvent = (...args: any[]) => void;
+export type StateMachineTransitionsConfig = StateMachineTransitionsConfigItem[];
+
+// This is passed to lifecycle transition events
+// added in initial config or via fsm.observe(...)
+export interface StateTransition {
+    transition: string;
+    from: string;
+    to: string;
+}
+
+export type StateTransitionMethod = (...args: any[]) => any;
+
+// tslint:disable-next-line void-return
+export type StateTransitionCallback = (lifecycle: StateTransition, ...args: any[]) => void | boolean | Promise<boolean | void>;
+
+export interface StateMethodsConfig {
+    [s: string]: StateTransitionCallback;
+}
 
 export interface StateMachineConfig {
-	initial?: any; // string or { state: 'foo', event: 'setup', defer: true|false }
-	events?: StateMachineEventDef[];
-	callbacks?: {
-		[s: string]: (event?: string, from?: string, to?: string, ...args: any[]) => any;
-	};
-	target?: StateMachine;
-	error?: StateMachineErrorCallback;
+    init?: string;
+    transitions?: StateMachineTransitionsConfig;
+    methods?: StateMethodsConfig;
 }
 
-export type StateMachineIsFinished = (state: string) => boolean;
-
-export const VERSION: string; 		        // = "2.4.0"
-export const WILDCARD: string;		        // = '*'
-export const ASYNC: string;			        // = 'async'
-
-export const Result: {
-	SUCCEEDED: number;	        // = 1, the event transitioned successfully from one state to another
-	NOTRANSITION: number;	    // = 2, the event was successfull but no state transition was necessary
-	CANCELLED: number;	        // = 3, the event was cancelled by the caller in a beforeEvent callback
-	PENDING: number;		        // = 4, the event is asynchronous and the caller is in control of when the transition occurs
-};
-
-export const Error: {
-	INVALID_TRANSITION: number;	// = 100, caller tried to fire an event that was innapropriate in the current state
-	PENDING_TRANSITION: number;	// = 200, caller tried to fire an event while an async transition was still pending
-	INVALID_CALLBACK: number;	// = 300, caller provided callback function threw an exception
-};
-
-export function create(config: StateMachineConfig, target?: StateMachine): StateMachine;
-
-export interface StateMachineTransition {
-	(): void;
-	cancel(): void;
+export interface StateMachineFactoryTarget {
+    _fsm(): void;
 }
 
 export type StateMachineIs = (state: string) => boolean;
-
-export type StateMachineCan = (evt: string) => boolean;
-
+export type StateMachineCan = (transition: string) => boolean;
+export type StateMachineCannot = (transition: string) => boolean;
 export type StateMachineTransitions = () => string[];
+export type StateMachineStates = () => string[];
+
+export type ObserveMethod = (transitionName: string, callback: StateTransitionCallback) => void;
+export type MultipleObserveMethod = (transitionName: string, callback: StateTransitionCallback) => void;
 
 export interface StateMachine {
-	current: string;
-	is: StateMachineIs;
-	can: StateMachineCan;
-	cannot: StateMachineCan;
-	error: StateMachineErrorCallback;
-	isFinished: StateMachineIsFinished;
-	/*  transition - only available when performing async state transitions; otherwise null. Can be a:
-		[1] fsm.transition(); // called from async callback
-		[2] fsm.transition.cancel();
-	*/
-	transition: StateMachineTransition;
-	transitions: StateMachineTransitions;
+    state: string;
+    data: Record<string, any>;
+    is: StateMachineIs;
+    can: StateMachineCan;
+    cannot: StateMachineCannot;
+    transitions: StateMachineTransitions;
+    allTransitions: StateMachineTransitions;
+    allStates: StateMachineStates;
+    observe: ObserveMethod | MultipleObserveMethod;
 }
 
+// tslint:disable-next-line no-unnecessary-class
+export class StateMachine {
+    constructor(config: StateMachineConfig);
+}
+
+// tslint:disable:no-unnecessary-generics
 export namespace StateMachine {
-	const VERSION: string; 		        // = "2.4.0"
-	const WILDCARD: string;		        // = '*'
-	const ASYNC: string;			        // = 'async'
-
-	const Result: {
-		SUCCEEDED: number;	        // = 1, the event transitioned successfully from one state to another
-		NOTRANSITION: number;	    // = 2, the event was successfull but no state transition was necessary
-		CANCELLED: number;	        // = 3, the event was cancelled by the caller in a beforeEvent callback
-		PENDING: number;		        // = 4, the event is asynchronous and the caller is in control of when the transition occurs
-	};
-
-	const Error: {
-		INVALID_TRANSITION: number;	// = 100, caller tried to fire an event that was innapropriate in the current state
-		PENDING_TRANSITION: number;	// = 200, caller tried to fire an event while an async transition was still pending
-		INVALID_CALLBACK: number;	// = 300, caller provided callback function threw an exception
-	};
-	function create(config: StateMachineConfig, target?: StateMachine): StateMachine;
+	function StateMachine<CustomMethodsInterface = any>(config: StateMachineConfig): StateMachine & CustomMethodsInterface;
+    function factory<CustomMethodsInterface = any>(config: StateMachineConfig): StateMachine & CustomMethodsInterface;
+	function factory<T, CustomMethodsInterface = any>(
+        target: new (config: StateMachineConfig) => T,
+        config: StateMachineConfig,
+	): StateMachine & T & CustomMethodsInterface;
+    function apply<T, CustomMethodsInterface = any>(
+        target: T,
+        config: StateMachineConfig,
+    ): StateMachine & T & CustomMethodsInterface;
 }
-
 export as namespace StateMachine;
--- types/javascript-state-machine/javascript-state-machine-tests.ts
+++ types/javascript-state-machine/javascript-state-machine-tests.ts
@@ -1,32 +1,98 @@
-import { StateMachine, StateMachineEvent, create } from 'javascript-state-machine';
+import * as assert from 'assert';
+import { StateMachine, StateTransitionMethod, StateMachineConfig, StateTransition } from 'javascript-state-machine';
 
-interface StateMachineTest extends StateMachine {
-	warn?: StateMachineEvent;
-	panic?: StateMachineEvent;
-	calm?: StateMachineEvent;
-	clear?: StateMachineEvent;
+interface MyStateMachineMethods {
+    warn: StateTransitionMethod;
+    panic: StateTransitionMethod;
+    calm: StateTransitionMethod;
+    clear: StateTransitionMethod;
 }
 
-const fsm: StateMachineTest = StateMachine.create({
-	initial: 'green',
-	events: [
-		{ name: 'warn', from: 'green', to: 'yellow' },
-		{ name: 'panic', from: 'yellow', to: 'red' },
-		{ name: 'calm', from: 'red', to: 'yellow' },
-		{ name: 'clear', from: 'yellow', to: 'green' }
-	],
-	callbacks: {
-		onpanic(event?, from?, to?, msg?) { alert('panic! ' + msg); },
-		onclear(event?, from?, to?, msg?) { alert('thanks to ' + msg); },
-		ongreen(event?, from?, to?) { document.body.className = 'green'; },
-		onyellow(event?, from?, to?) { document.body.className = 'yellow'; },
-		onred(event?, from?, to?) { document.body.className = 'red'; },
-	}
-});
+// we will assert this variable - basically
+// a super basic way to spy
+let spyLastMethodCalled = '';
+const config: StateMachineConfig = {
+    init: 'green',
+    transitions: [
+        { name: 'warn', from: 'green', to: 'yellow' },
+        { name: 'panic', from: 'yellow', to: 'red' },
+        { name: 'calm', from: 'red', to: 'yellow' },
+        { name: 'clear', from: 'yellow', to: 'green' },
+    ],
+    methods: {
+        onWarn(lifecycle: StateTransition, testArgument: string) {
+            spyLastMethodCalled = `onWarn:${testArgument}`;
+        },
+        onPanic(lifecycle: StateTransition, testArgument: string) {
+            spyLastMethodCalled = `onPanic:${testArgument}`;
+        },
+        onCalm(lifecycle: StateTransition, testArgument: string) {
+            spyLastMethodCalled = `onCalm:${testArgument}`;
+        },
+        onClear(lifecycle: StateTransition, testArgument: string) {
+            spyLastMethodCalled = `onClear:${testArgument}`;
+        },
+    },
+};
 
-// fsm.warn();					// transition from green to yellow
-// fsm.panic("ERROR ALERT");	// transition from yellow to red
-// fsm.calm();					// transition from red to yellow
-// fsm.clear("All clear");		// transition from yellow to green
+const fsm = new StateMachine(config) as MyStateMachineMethods & StateMachine;
+assert(fsm.state.includes('green'));
+fsm.warn();
+assert(fsm.state.includes('yellow'));
+assert(spyLastMethodCalled.includes('onWarn'));
+
+// Use factory without custom typing
+const fsmFactoryUntyped = StateMachine.factory(config);
+fsmFactoryUntyped.panic('oops');
+assert(fsm.state.includes('red'));
+assert(spyLastMethodCalled.includes('onPanic:oops'));
+assert(fsmFactoryUntyped.notAMethod === undefined);
+
+// Use factory with custom typing
+const fsmFactoryTyped = StateMachine.factory<MyStateMachineMethods>(config);
+assert(fsmFactoryTyped.state === 'green');
+fsmFactoryTyped.calm('yay');
+assert(fsm.state.includes('yellow'));
+assert(spyLastMethodCalled.includes('onCalm:yay'));
+
+interface MyObjectInterface {
+    props: boolean;
+}
+
+const myObject: MyObjectInterface = {
+    props: true,
+};
+const objectWithFsmUntyped = StateMachine.apply(myObject, config);
+assert(objectWithFsmUntyped.state === 'green');
+assert(objectWithFsmUntyped.props === true);
+objectWithFsmUntyped.calm();
+assert(spyLastMethodCalled.includes('onCalm'));
+assert(fsm.state.includes('green'));
+
+interface MyClass {
+    _fsm(): void;
+}
+class MyClass {
+    props: boolean;
+    constructor() {
+        this.props = true;
+        this._fsm();
+    }
+}
+
+const classInstanceWithFsmUntyped = StateMachine.factory(MyClass, config);
+assert(classInstanceWithFsmUntyped.state === 'green');
+assert(classInstanceWithFsmUntyped.props);
+classInstanceWithFsmUntyped.panic();
+assert(spyLastMethodCalled.includes('panic'));
+assert(fsm.state.includes('red'));
+
+const classInstanceWithFsmTyped = StateMachine.factory<MyClass, MyStateMachineMethods>(MyClass, config);
+assert(classInstanceWithFsmTyped.state === 'green');
+assert(classInstanceWithFsmTyped.props);
+classInstanceWithFsmTyped.warn('be careful!');
+assert(spyLastMethodCalled.includes('warn:be careful!'));
+assert(fsm.state.includes('yellow'));
 
 const transitions = fsm.transitions();
+assert(transitions.length === 2);
